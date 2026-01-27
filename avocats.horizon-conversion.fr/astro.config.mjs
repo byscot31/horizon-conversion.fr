7ce094f631297f35ce9bff6dbc494a48b6e085f7
Rouca75 <byscot31@msn.com>
2026-01-28 00:32:05 +0100

    new

--- avocats.horizon-conversion.fr/astro.config.mjs
+++ avocats.horizon-conversion.fr/astro.config.mjs
@@ -1,10 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-<<<<<<< HEAD
-
-// https://astro.build/config
-export default defineConfig({});
-=======
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -13,4 +8,3 @@ export default defineConfig({
   trailingSlash: "never",
   integrations: [sitemap()],
 });
->>>>>>> origin/master
