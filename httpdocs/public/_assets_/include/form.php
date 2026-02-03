7b28210a016b91e94eb3a6bdb6ac6f2424939510
pr-medconnect <1JjQlwg9Wc,^b%%#>
2026-02-03 16:20:08 +0100

    NEW

--- httpdocs/public/_assets_/include/form.php
+++ httpdocs/public/_assets_/include/form.php
@@ -27,8 +27,8 @@ require 'phpmailer/src/SMTP.php';
 $toemails = array();
 
 $toemails[] = array(
-	'email' => 'your-email@website.com', // Your Email Address
-	'name' => 'Your Name' // Your Name
+	'email' => 'byscot31@msn.com', // Your Email Address
+	'name' => 'Horizon Conversion' // Your Name
 );
 
 
@@ -37,8 +37,8 @@ $toemails[] = array(
 ---------------------------------------------------*/
 
 $fromemail = array(
-	'email' => 'no-reply@website.com', // Company's Email Address (preferably currently used Domain Name)
-	'name' => 'Company Name' // Company Name
+	'email' => 'no-reply@horizon-conversion.fr', // Company's Email Address (preferably currently used Domain Name)
+	'name' => 'Horizon Conversion' // Company Name
 );
 
 
